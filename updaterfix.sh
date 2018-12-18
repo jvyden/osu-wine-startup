a0e0c6e2958b41dad5308c6b6880580b5d2b3da5
jvyden420 <jvyden420@gmail.com>
2018-12-18 18:17:24 -0500

    Fix incorrect package name in updater fixing script.

--- updaterfix.sh
+++ updaterfix.sh
@@ -4,7 +4,7 @@ cd gnutlsfix
 wget http://archive.ubuntu.com/ubuntu/pool/main/g/gnutls28/libgnutls30_3.5.18-1ubuntu1_amd64.deb
 wget http://archive.ubuntu.com/ubuntu/pool/main/g/gnutls28/libgnutls30_3.5.18-1ubuntu1_i386.deb
 dpkg -i libgnutls30_3.5.18-1ubuntu1_amd64.deb libgnutls30_3.5.18-1ubuntu1_i386.deb
-apt-mark hold libgnutls30_3:i386
-apt-mark hold libgnutls30_3:amd64
+apt-mark hold libgnutls30:i386
+apt-mark hold libgnutls30:amd64
 cd ..
 rm -rf gnutlsfix
